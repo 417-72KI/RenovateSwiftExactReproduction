bdbe8cd61f1f769cc0ef507b2d14b719668d909e
417-72KI <417.72ki@gmail.com>
2024-03-05 14:48:23 +0900

    add package to reproduce

---
+++ Package.swift
@@ -0,0 +1,25 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "RenovateSwiftExactReproduction",
+    products: [
+        .library(
+            name: "RenovateSwiftExactReproduction",
+            targets: ["RenovateSwiftExactReproduction"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.8.0"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk", from: "10.21.0"),
+        .package(url: "https://github.com/apple/swift-testing", exact: "0.4.1"),
+        .package(url: "https://github.com/realm/SwiftLint", .exact("0.53.0")),
+    ],
+    targets: [
+        .target(
+            name: "RenovateSwiftExactReproduction"),
+        .testTarget(
+            name: "RenovateSwiftExactReproductionTests",
+            dependencies: ["RenovateSwiftExactReproduction"]),
+    ]
+)
